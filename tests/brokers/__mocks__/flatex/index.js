48890a6846e8f04b71ae2b9e83eab6422522c7cd
Florian Karner <mail@floriankarner.at>
2020-05-11 23:43:28 +0200

    updated flatex mocks

---
+++ tests/brokers/__mocks__/flatex/index.js
@@ -0,0 +1,10 @@
+export const buySamples = [
+    require('./buy1.json'),
+    require('./buy2_etf.json'),
+    require('./buy3.json'),
+    require('./buy4.json'),
+];
+
+export const dividendsSamples  = [
+    require('./dividend1.json'),
+];
