6693a972e427fb51185a17b8f676a53f0b941c6c
Moritz Körber <koerber.moritz@gmail.com>
2020-05-14 18:04:08 +0200

    Add ing to brokers

---
+++ tests/brokers/__mocks__/ing/index.js
@@ -0,0 +1,19 @@
+export const buySamples = [
+  require('./buy1.json'),
+  require('./buy2.json'),
+  require('./buy_plan.json'),
+];
+
+export const sellSamples  = [
+  require('./sell1.json'),
+  require('./sell2.json'),
+];
+
+export const dividendsSamples  = [
+  require('./dividend1.json'),
+  require('./dividend2.json'),
+];
+
+export const invalidSamples  = [
+  require('./invalid_buy.json'),
+];
