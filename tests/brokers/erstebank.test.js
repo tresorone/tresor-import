4d836cc267608f75d8e2a1190675581935c61693
Thomas Eitler <thomas.eitler@pm.me>
2020-06-16 17:59:35 +0200

    rename variables for testtables to better match javascript naming best practices

--- tests/brokers/erstebank.test.js
+++ tests/brokers/erstebank.test.js
@@ -28,7 +28,7 @@ const samples = buySamples.concat(sellSamples, dividendsSamples);
 
 // INTERNAL FUNCTIONS
 describe('Test internal functions for Erste Bank Parser', () => {
-  const isBuy_testcases = [
+  const TTisBuy = [
     [samples[0], true],
     [samples[1], true],
     [samples[2], true],
@@ -42,14 +42,14 @@ describe('Test internal functions for Erste Bank Parser', () => {
     [samples[10], false],
     [samples[11], false],
   ];
-  test.each(isBuy_testcases)(
+  test.each(TTisBuy)(
     'should identify Buy Statements in Sample %#',
     (sample, expected) => {
       expect(isBuy(sample)).toEqual(expected);
     }
   );
 
-  const isDividend_testcases = [
+  const TTisDividend = [
     [samples[0], false],
     [samples[1], false],
     [samples[2], false],
@@ -63,14 +63,14 @@ describe('Test internal functions for Erste Bank Parser', () => {
     [samples[10], true],
     [samples[11], true],
   ];
-  test.each(isDividend_testcases)(
+  test.each(TTisDividend)(
     'should identify Dividend Statements in Sample %#',
     (sample, expected) => {
       expect(isDividend(sample)).toEqual(expected);
     }
   );
 
-  const isin_testcases = [
+  const TTfindISIN = [
     [samples[0], 'AT0000A05HR3'],
     [samples[1], 'AT0000A08SH5'],
     [samples[2], 'AT0000660600'],
@@ -81,14 +81,14 @@ describe('Test internal functions for Erste Bank Parser', () => {
     [samples[7], 'AT0000680970'],
     [samples[8], 'AT0000A296E8'],
   ];
-  test.each(isin_testcases)(
+  test.each(TTfindISIN)(
     'should be able to findISIN in Sample %#',
     (sample, expected) => {
       expect(findISIN(sample, 1)).toBe(expected);
     }
   );
 
-  const company_testcases = [
+  const TTfindCompany = [
     [samples[0], 'ERSTE BOND EMERGING MARKETS CORPOR.'],
     [samples[1], 'ERSTE IMMOBILIENFONDS'],
     [samples[2], 'ESPA SELECT BOND (T)'],
@@ -99,14 +99,14 @@ describe('Test internal functions for Erste Bank Parser', () => {
     [samples[7], 'ERSTE STOCK EM GLOBAL EUR R T'],
     [samples[8], 'ERSTE FUTURE INVEST EUR R T'],
   ];
-  test.each(company_testcases)(
+  test.each(TTfindCompany)(
     'should be able to findCompany in Sample %#',
     (sample, expected) => {
       expect(findCompany(sample, 1)).toBe(expected);
     }
   );
 
-  const parseGermanNum_testcases = [
+  const TTparseGermanNum = [
     ['99.735,77', 99735.77],
     ['99.833,64', 99833.64],
     ['123456,50', 123456.5],
@@ -123,14 +123,14 @@ describe('Test internal functions for Erste Bank Parser', () => {
     ['hallo', NaN],
     ['Tresor', NaN],
   ];
-  test.each(parseGermanNum_testcases)(
+  test.each(TTparseGermanNum)(
     'should be able to parseGermanNum in Sample %#',
     (sample, expected) => {
       expect(parseGermanNum(sample)).toBe(expected);
     }
   );
 
-  const buyselldate_testcases = [
+  const TTfindDateBuySell = [
     [samples[0], '02.03.2017'],
     [samples[1], '01.03.2017'],
     [samples[2], '02.03.2017'],
@@ -141,14 +141,13 @@ describe('Test internal functions for Erste Bank Parser', () => {
     [samples[7], '12.05.2020'],
     [samples[8], '12.05.2020'],
   ];
-  test.each(buyselldate_testcases)(
+  test.each(TTfindDateBuySell)(
     'should be able to findDateBuySell in Sample %#',
     (sample, expected) => {
       expect(findDateBuySell(sample)).toBe(expected);
     }
   );
 
-  const amount_testcases = [
     [samples[0], 99735.77],
     [samples[1], 99833.64],
     [samples[2], 99637.5],
@@ -158,8 +157,9 @@ describe('Test internal functions for Erste Bank Parser', () => {
     [samples[6], 99086.64],
     [samples[7], 9505.9],
     [samples[8], 99289.02],
+  const TTfindAmount = [
   ];
-  test.each(amount_testcases)(
+  test.each(TTfindAmount)(
     'should be able to findAmount in Sample %#',
     (sample, expected) => {
       expect(findAmount(sample)).toBe(expected);
@@ -173,7 +173,7 @@ describe('Test internal functions for Erste Bank Parser', () => {
     }
   );
 
-  const fee_testcases = [
+  const TTfindFee = [
     [samples[0], 0],
     [samples[1], 0],
     [samples[2], 0],
@@ -184,43 +184,40 @@ describe('Test internal functions for Erste Bank Parser', () => {
     [samples[7], 234.21],
     [samples[8], 550.4],
   ];
-  test.each(fee_testcases)(
-    'should find fees in Sample %#',
-    (sample, expected) => {
-      expect(findFee(sample)).toEqual(expected);
-    }
-  );
+  test.each(TTfindFee)('should find fees in Sample %#', (sample, expected) => {
+    expect(findFee(sample)).toEqual(expected);
+  });
 
-  const divdate_testcases = [
+  const TTfindDividendDate = [
     [samples[9], '03.07.2017'],
     [samples[10], '02.07.2018'],
     [samples[11], '01.07.2019'],
   ];
-  test.each(divdate_testcases)(
+  test.each(TTfindDividendDate)(
     'should be able to findDividendDate in Sample %#',
     (sample, expected) => {
       expect(findDividendDate(sample)).toBe(expected);
     }
   );
 
-  const divshare_testcases = [
+  const TTfindDividendShares = [
     [samples[9], 138],
     [samples[10], 138],
     [samples[11], 138],
   ];
-  test.each(divshare_testcases)(
+  test.each(TTfindDividendShares)(
     'should be able to findDividendShares in Sample %#',
     (sample, expected) => {
       expect(findDividendShares(sample)).toBe(expected);
     }
   );
 
-  const divpayout_testcases = [
     [samples[9], 205.11],
     [samples[10], 203.3],
     [samples[11], 202.58],
+  const TTfindDividendPayout = [
   ];
-  test.each(divpayout_testcases)(
+  test.each(TTfindDividendPayout)(
     'should be able to findDividendPayout in Sample %#',
     (sample, expected) => {
       expect(findDividendPayout(sample)).toBe(expected);
