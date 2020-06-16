238c7f4a00e46e0201e132b0f4123ea8187daee2
Thomas Eitler <thomas.eitler@pm.me>
2020-06-16 15:17:21 +0200

    add anonymized sample files for erstebank testcases

---
+++ tests/brokers/__mocks__/erstebank/index.js
@@ -0,0 +1,20 @@
+export const buySamples = [
+  require('./2017_buy_0.json'),
+  require('./2017_buy_1.json'),
+  require('./2017_buy_2.json'),
+  require('./2017_buy_3.json'),
+  require('./2020_buy_0.json'),
+  require('./2020_buy_1.json'),
+  require('./2020_buy_2.json'),
+  require('./2020_buy_3.json'),
+  require('./2020_buy_4.json'),
+];
+
+export const sellSamples = [];
+export const dividendsSamples = [
+  require('./2017_div.json'),
+  require('./2018_div.json'),
+  require('./2019_div.json'),
+];
+
+// export const invalidSamples = [require('./invalid_buy.json')];
