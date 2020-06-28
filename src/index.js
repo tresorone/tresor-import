da124955a9a6b2957a2615166205bd4746b3be96
David Dreher <dredav@users.noreply.github.com>
2020-06-28 13:16:06 +0200

    Log page content to console

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const getActivity = contents => {
   // identify broker from first page
   const broker = getBroker(contents[0]);
 
+  console.log(JSON.stringify(contents, null, 2));
+
   if (broker) {
     // get activities from all pages
     return broker.parsePages(contents);
