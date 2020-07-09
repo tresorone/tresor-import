6d8d4890b85b3a5047744f6d14457ac669d2034f
Felix Heck <hi@whotheheck.de>
2020-07-09 08:52:39 +0200

    add test mock data

---
+++ tests/brokers/__mocks__/consorsbank/index.js
@@ -0,0 +1,14 @@
+export const buySamples = [
+  require('./buy1.json'),
+  require('./buy2.json'),
+  require('./buy3.json'),
+];
+
+export const sellSamples = [require('./sell1.json'), require('./sell2.json')];
+
+export const dividendsSamples = [
+  require('./div1.json'),
+  require('./div2.json'),
+  require('./div3.json'),
+  require('./div4.json'),
+];
