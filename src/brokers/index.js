e6603d542faa321ba9ff70ebfde2d736cf76c1d3
David Dreher <dredav@users.noreply.github.com>
2020-08-30 08:36:27 +0200

    Add broker 1822direkt. Fix #58

--- src/brokers/index.js
+++ src/brokers/index.js
@@ -6,5 +6,16 @@ import * as ing from './ing';
 import * as flatex from './flatex';
 import * as pb from './pb';
 import * as consorsbank from './consorsbank';
+import * as _1822direkt from './1822direkt';
 
-export { comdirect, dkb, traderepublic, onvista, ing, pb, flatex, consorsbank };
+export {
+  comdirect,
+  dkb,
+  traderepublic,
+  onvista,
+  ing,
+  pb,
+  flatex,
+  consorsbank,
+  _1822direkt,
+};
