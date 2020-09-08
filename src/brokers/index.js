91d5bbb5ab368f31f4688b2ef23da37c0065b7ec
dho <david-christian.holin@t-systems.com>
2020-09-08 14:00:01 +0200

    Implemented a broker that handles eBase depot exports. The file it expects is the >Umsatzdetails as PDF< File, downloadable through the WebUI. A broker for the csv Variant that offers more details will be delivered later on. Sadly, i do not have access to detailed test-files so the testing is limited so far. This is mentioned in the TODOs. Furthermore, the PDF does not include details about fees and taxes. These information are only present in the pdf

--- src/brokers/index.js
+++ src/brokers/index.js
@@ -1,10 +1,21 @@
 import * as comdirect from './comdirect';
-import * as dkb from './dkb';
-import * as traderepublic from './traderepublic';
-import * as onvista from './onvista';
-import * as ing from './ing';
-import * as flatex from './flatex';
-import * as pb from './pb';
 import * as consorsbank from './consorsbank';
+import * as dkb from './dkb';
+import * as ebase from './ebase';
+import * as flatex from './flatex';
+import * as ing from './ing';
+import * as onvista from './onvista';
+import * as pb from './pb';
+import * as traderepublic from './traderepublic';
 
-export { comdirect, dkb, traderepublic, onvista, ing, pb, flatex, consorsbank };
+export {
+  comdirect,
+  dkb,
+  ebase,
+  traderepublic,
+  onvista,
+  ing,
+  pb,
+  flatex,
+  consorsbank
+};
---
+++ tests/brokers/__mocks__/ebase/index.js
@@ -0,0 +1,13 @@
+export const buySamples = [
+    require('./buy-1.json')
+];
+
+export const sellSamples = [
+    require('./sell-1.json')
+];
+
+/* I do not have access to a dividen sample atm. Skipping for now. (David Holin)
+export const dividendsSamples  = [
+    require('./div-1.json'),
+];
+*/
