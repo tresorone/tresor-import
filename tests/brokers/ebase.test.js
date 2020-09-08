c8981fc6d33e36f5b0e33f001cfe131c49c1b736
dho <david-christian.holin@t-systems.com>
2020-09-08 15:17:26 +0200

    Added a more tests and implemented another case that might occur in the imported PDF.

--- tests/brokers/ebase.test.js
+++ tests/brokers/ebase.test.js
@@ -1,10 +1,40 @@
 import { getBroker } from '@/index';
 import * as ebase from '../../src/brokers/ebase';
-import { buySamples, sellSamples } from './__mocks__/ebase';
+import { buySamples, invalidSamples, mixedSamples, sellSamples } from './__mocks__/ebase';
+import { canParseData, parseData } from '@/brokers/ebase';
 
 // David Holin: No dividend samples test yet, as no example document is available
 describe('Broker: ebase', () => {
   let consoleErrorSpy;
+
+  test('should only accept revenue-summary reports', () => {
+    expect(canParseData(['Fondsertrag / Vorabpauschale'])).toEqual(
+      true
+    );
+  });
+
+  test('should reject unknown PDF files', () => {
+    expect(canParseData(['This String should never occur in a legitimate document'])).toEqual(
+      false
+    );
+  });
+
+  test('should validate the result', () => {
+    const activity = parseData(invalidSamples[0]);
+
+    expect(activity).toEqual(undefined);
+    expect(console.error).toHaveBeenLastCalledWith('Error while parsing PDF', {
+      amount: 45,
+      broker: 'ebase',
+      company: 'DWS Top Dividende LD',
+      date: '2019-12-19',
+      fee: 0,
+      isin: 'DE0009848119',
+      price: 130.93,
+      shares: NaN,
+      type: 'Sell'
+    });
+  });
 
   describe('Check all documents', () => {
     test('Can parse one page containing sell orders with ebase', () => {
@@ -44,7 +74,54 @@ describe('Broker: ebase', () => {
         shares: 0.126761,
         price: 120.7,
         amount: 15.3,
-        fee: 0.0,
+        fee: 0.0
+      });
+    });
+  });
+
+  describe('Validate sells', () => {
+    test('Can parse multiple sell orders from a document', () => {
+      const activities = ebase.parsePages(sellSamples[0]);
+      expect(activities.length).toEqual(2);
+      expect(activities[0]).toEqual({
+        broker: 'ebase',
+        type: 'Sell',
+        date: '2019-12-19',
+        isin: 'DE0009848119',
+        company: 'DWS Top Dividende LD',
+        shares: 0.343695,
+        price: 130.93,
+        amount: 45.0,
+        fee: 0.0
+      });
+      expect(activities[1]).toEqual({
+        broker: 'ebase',
+        type: 'Sell',
+        date: '2018-12-19',
+        isin: 'DE0009848119',
+        company: 'DWS Top Dividende LD',
+        shares: 0.394046,
+        price: 114.2,
+        amount: 45.0,
+        fee: 0.0
+      });
+    });
+  });
+
+  describe('Mixed Sells, buys and everything in between', () => {
+    test('Can parse multiple sell orders from a document', () => {
+      const activities = ebase.parsePages(mixedSamples[0]);
+      expect(activities.length).toEqual(316);
+      expect(activities[0]).toEqual({
+        broker: 'ebase',
+        type: 'Buy',
+        date: '2020-07-01',
+        isin: 'DE000A0X7541',
+        company: 'ACATIS GANÉ VALUE EVENT FONDS A',
+        shares: 0.054571,
+        price: 311.52,
+        amount: 17.0,
+        fee: 0.0
       });
     });
   });
