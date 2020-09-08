3a9bc6266ebe6fe1b9338fa748bfd6b97d3bda23
dho <david-christian.holin@t-systems.com>
2020-09-08 15:17:26 +0200

    Added a more tests and implemented another case that might occur in the imported PDF.

--- tests/brokers/ebase.test.js
+++ tests/brokers/ebase.test.js
@@ -77,7 +77,54 @@ describe('Broker: ebase', () => {
         shares: 0.126761,
         price: 120.7,
         amount: 15.3,
-        fee: 0.0,
+        fee: 0.0
+      });
+    });
+  });
+
+  describe('Validate sells', () => {
+    test('Can parse multiple sell orders from a document', () => {
+      const activities = ebase.parsePages(sellSamples[0]);
+      expect(activities.length).toEqual(2);
+      expect(activities[0]).toEqual({
+        broker: 'ebase',
+        type: 'Sell',
+        date: '2019-12-19',
+        isin: 'DE0009848119',
+        company: 'DWS Top Dividende LD',
+        shares: 0.343695,
+        price: 130.93,
+        amount: 45.0,
+        fee: 0.0
+      });
+      expect(activities[1]).toEqual({
+        broker: 'ebase',
+        type: 'Sell',
+        date: '2018-12-19',
+        isin: 'DE0009848119',
+        company: 'DWS Top Dividende LD',
+        shares: 0.394046,
+        price: 114.2,
+        amount: 45.0,
+        fee: 0.0
+      });
+    });
+  });
+
+  describe('Mixed Sells, buys and everything in between', () => {
+    test('Can parse multiple sell orders from a document', () => {
+      const activities = ebase.parsePages(mixedSamples[0]);
+      expect(activities.length).toEqual(316);
+      expect(activities[0]).toEqual({
+        broker: 'ebase',
+        type: 'Buy',
+        date: '2020-07-01',
+        isin: 'DE000A0X7541',
+        company: 'ACATIS GANÉ VALUE EVENT FONDS A',
+        shares: 0.054571,
+        price: 311.52,
+        amount: 17.0,
+        fee: 0.0
       });
     });
   });
