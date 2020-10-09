cc19fb2a450751150a2fcb0a1d8bda67d27efe48
David Christian Holin <61882185+SirGibihm@users.noreply.github.com>
2020-10-09 20:16:39 +0200

    This offers a parsers for the .pdf ebase files. Dividends are not yet included, neither is the parser for .csv ebase exports. This replaces an older pull request that was a mess.

---
+++ src/brokers/ebase.js
@@ -0,0 +1,157 @@
+import format from 'date-fns/format';
+import parse from 'date-fns/parse';
+import Big from 'big.js';
+import { parseGermanNum, validateActivity } from '@/helper';
+
+const parseShare = shareString => {
+  try {
+    return +Big(parseGermanNum(shareString)).abs();
+  } catch (e) {
+    if (e.message === '[big.js] Invalid number') {
+      return undefined;
+    } else {
+      throw e; // re-throw the error unchanged
+    }
+  }
+};
+
+const parsePrice = priceString => {
+  try {
+    return +Big(parseGermanNum(priceString.split(' ')[0])).abs();
+  } catch (e) {
+    if (e.message === '[big.js] Invalid number') {
+      return undefined;
+    } else {
+      throw e; // re-throw the error unchanged
+    }
+  }
+};
+
+const parseAmount = amountString => {
+  try {
+    return +Big(parseGermanNum(amountString.split(' ')[0])).abs();
+  } catch (e) {
+    if (e.message === '[big.js] Invalid number') {
+      return undefined;
+    } else {
+      throw e; // re-throw the error unchanged
+    }
+  }
+};
+
+function verifyActivity(
+  type,
+  date,
+  isin,
+  company,
+  shares,
+  price,
+  amount,
+  tax,
+  fee
+) {
+  const activity = {
+    broker: 'ebase',
+    type,
+    date: format(parse(date, 'dd.MM.yyyy', new Date()), 'yyyy-MM-dd'),
+    isin,
+    company,
+    shares,
+    price,
+    amount,
+    tax,
+    fee,
+  };
+  return validateActivity(activity);
+}
+
+function parseBaseAction(pdfArray, i, actionType) {
+  const type = actionType;
+  const company = pdfArray[i + 1];
+  const isin = pdfArray[i + 2];
+  const shares = parseShare(pdfArray[i + 4]);
+  const price = parsePrice(pdfArray[i + 5]);
+  const date = pdfArray[i + 6];
+  const amount = parseAmount(pdfArray[i + 7]);
+  const tax = 0;
+  const fee = 0;
+  return verifyActivity(
+    type,
+    date,
+    isin,
+    company,
+    shares,
+    price,
+    amount,
+    tax,
+    fee
+  );
+}
+
+export const canParseData = (textArr) =>
+  (textArr.some(t => t.includes('ebase Depot flex standard')) &&
+  textArr.some(line => line.includes('Fondsertrag / Vorabpauschale')));
+
+export const parseData = pdfPages => {
+  // Action can be: Fondsertrag (Ausschüttung), Ansparplan, Wiederanlage Fondsertrag, Entgelt Verkauf
+  let actions = [];
+  for (const pdfPage of pdfPages) {
+    let i = 0;
+
+    while (i <= pdfPage.length) {
+      if (pdfPage[i] === 'Ansparplan') {
+        const action = parseBaseAction(pdfPage, i, 'Buy');
+        if (action === undefined) {
+          return undefined;
+        }
+        actions.push(action);
+        // An 'Ansparplan'/'Wiederanlage Fondsertrag' entry occupies 7 array entries.
+        i += 6;
+      }
+      else if (pdfPage[i] === 'Wiederanlage Fondsertrag') {
+        const action = parseBaseAction(pdfPage, i, 'Sell');
+        if (action === undefined) {
+          return undefined;
+        }
+        actions.push(action);
+        // An 'Ansparplan'/'Wiederanlage Fondsertrag' entry occupies 7 array entries.
+        i += 6;
+      }
+      else if (pdfPage[i] === 'Fondsertrag (Ausschüttung)') {
+        // This was always blank in the example files I had -> So no parsing could be done.
+        i += 3;
+      }
+      else if (pdfPage[i] === 'Entgelt Verkauf') {
+        const action = parseBaseAction(pdfPage, i, 'Sell');
+        if (action === undefined) {
+          return undefined;
+        } // An 'Entgelt Verkauf' entry occupies 9 array entries.
+        actions.push(action);
+        i += 8;
+      }
+      else if (pdfPage[i] === 'Verkauf') {
+        const action = parseBaseAction(pdfPage, i, 'Sell');
+        if (action === undefined) {
+          return undefined;
+        }
+        actions.push(action);
+        i += 8; // A basic 'Verkauf' entry occupies 9 array entries in total
+      }
+      else if (pdfPage[i] === 'Vorabpauschale') {
+        // This was always blank in the example files I had -> So no parsing could be done.
+        i += 3;
+      }
+      i++;
+    }
+  }
+  return actions;
+};
+
+export const canParsePage = (content, extension) =>
+  extension === 'pdf' &&
+  (content.some(line => line.includes('ebase Depot flex standard')) &&
+  content.some(line => line.includes('Fondsertrag / Vorabpauschale')))
+
+export const parsePages = contents => {
+  return parseData(contents);
+};
--- src/brokers/index.js
+++ src/brokers/index.js
@@ -1,21 +1,23 @@
 import * as comdirect from './comdirect';
-import * as dkb from './dkb';
-import * as traderepublic from './traderepublic';
-import * as onvista from './onvista';
-import * as ing from './ing';
-import * as flatex from './flatex';
-import * as pb from './postbank';
 import * as consorsbank from './consorsbank';
+import * as ebase from './ebase';
+import * as flatex from './flatex';
+import * as dkb from './dkb';
+import * as ing from './ing';
+import * as onvista from './onvista';
+import * as pb from './postbank';
 import * as scalableCapital from './scalableCapital';
+import * as traderepublic from './traderepublic';
 
 export {
   comdirect,
-  dkb,
-  traderepublic,
-  onvista,
-  ing,
-  pb,
-  flatex,
   consorsbank,
+  ebase,
+  dkb,
+  flatex,
+  ing,
+  onvista,
+  pb,
   scalableCapital,
+  traderepublic,
 };
