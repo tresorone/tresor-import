f91c7f61eac389cf9c7a185c095b8a96062cee56
David Christian Holin <61882185+SirGibihm@users.noreply.github.com>
2020-10-19 19:17:37 +0200

    One of the >Sell< tests was located at the buy section in the tests. Corrected this.

--- tests/brokers/__mocks__/dkb/index.js
+++ tests/brokers/__mocks__/dkb/index.js
@@ -1,17 +1,18 @@
 export const buySamples = [
-    require('./buy-1.json'),
-    require('./buy-2.json'),
-    require('./buy-3.json'),
-    require('./sell_stop_market_order_invesco_msci_world.json'),
+  require('./buy-1.json'),
+  require('./buy-2.json'),
+  require('./buy-3.json'),
 ];
 
-export const sellSamples  = [
-    require('./sell-1.json'),
+export const sellSamples = [
+  require('./sell/sell-1.json'),
+  require('./sell/sell_stop_market_order_invesco_msci_world.json'),
+  require('./sell/sell_etf_IE00B4L5Y983.json'),
 ];
 
-export const dividendsSamples  = [
-    require('./div-1.json'),
-    require('./div-2.json'),
-    require('./div-3.json'),
-    require('./div-4.json'),
+export const dividendsSamples = [
+  require('./div-1.json'),
+  require('./div-2.json'),
+  require('./div-3.json'),
+  require('./div-4.json'),
 ];
