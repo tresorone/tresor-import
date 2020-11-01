d3b8f9e2cfc46c954f76dc8de0a332650600c33e
David Christian Holin <61882185+SirGibihm@users.noreply.github.com>
2020-11-01 23:15:16 +0100

    Implemented the testfiles given in #54. Testcase buy_2017_1 seems to be phishy and there is some code-cleanup left to do (!!!). Also, in canParsePage there is an evil workaround

--- src/brokers/ersteBank.js
+++ src/brokers/ersteBank.js
@@ -4,22 +4,30 @@ import Big from 'big.js';
 
 import { parseGermanNum, validateActivity } from '@/helper';
 
+const stringAtIndexOf = (pageArray, indexOf, inLineIndex, lineOffset = 0) => {
+  const lineIndex = pageArray.indexOf(indexOf) + lineOffset;
+  if (lineIndex >= 0) {
+    return pageArray[lineIndex].split(/\s+/)[inLineIndex];
+  }
+};
+
 const findForeignCurrencyFxRate = pageArray => {
-  const foreignIndex = pageArray.findIndex(line => line.includes('umgerechneter FW - Endwert'));
-  let foreignCurrency, fxRate
-  if ( foreignIndex > 0 ) {
+  const foreignIndex = pageArray.findIndex(line =>
+    line.includes('umgerechneter FW - Endwert')
+  );
+  let foreignCurrency, fxRate;
+  if (foreignIndex > 0) {
     const foreignIndexLine = pageArray[foreignIndex - 1].split(' ');
-    fxRate = parseGermanNum(foreignIndexLine[2])
+    fxRate = parseGermanNum(foreignIndexLine[2]);
     foreignCurrency = foreignIndexLine[0].split('-')[0];
   }
   return [fxRate, foreignCurrency];
-}
+};
 
 const isForeignCurrencyTransaction = pageArray =>
   pageArray.some(line => line.includes('umgerechneter FW - Endwert'));
 
-
-const findIsin = pageArray => {
+const findIsinBuy = pageArray => {
   const isinIndex =
     pageArray.findIndex(line => line.includes('Ihr Auftrag vom')) + 1;
   return pageArray[isinIndex].split(/\s/)[0];
@@ -28,17 +36,32 @@ const findIsin = pageArray => {
 const findCompany = pageArray => {
   const companyIndex =
     pageArray.findIndex(line => line.includes('Ihr Auftrag vom')) + 1;
-  return pageArray[companyIndex].split(/(\s+)/).slice(2).join('');
+  if (companyIndex >= 0) {
+    return pageArray[companyIndex].split(/(\s+)/).slice(2).join('');
+  }
 };
 
-const findDate = pageArray => {
+const findCompanyDividend = pageArray => {
+  const companyIndex = pageArray.indexOf('Ausschüttung') + 1;
+  if (companyIndex >= 0) {
+    return pageArray[companyIndex].split(/(\s+)/).slice(2).join('');
+  }
+};
+
+const findDateBuy = pageArray => {
   const dateIndex =
     pageArray.findIndex(line => line.includes('Verrechnungskonto')) - 1;
   const date = pageArray[dateIndex].split(/(\s+)/)[6];
   return format(parse(date, 'dd.MM.yyyy', new Date()), 'yyyy-MM-dd');
 };
 
-const findAmount = pageArray => {
+const findDateDividend = pageArray => {
+  const dateIndex = pageArray.findIndex(line => line.includes('mit Valuta'));
+  const date = pageArray[dateIndex].split(/(\s+)/)[22];
+  return format(parse(date, 'dd.MM.yyyy', new Date()), 'yyyy-MM-dd');
+};
+
+const findAmountBuy = (pageArray, legacyDocument = false) => {
   // Two occurrences of Kurswert are expected; we are interested in the first
   let amountLineIndex =
     pageArray.findIndex(line => line.includes('Kurswert')) + 1;
@@ -46,93 +69,186 @@ const findAmount = pageArray => {
   if (isForeignCurrencyTransaction(pageArray)) {
     amountLineIndex += 2;
   }
-  const amount = pageArray[amountLineIndex].split(/\s+/)[4];
+  // In older Documents the position of the amount is slightly different
+  const amountInLinePosition = legacyDocument ? 6 : 4;
+  const amount = pageArray[amountLineIndex].split(/\s+/)[amountInLinePosition];
   return parseGermanNum(amount);
 };
 
-const findShares = pageArray => {
+const findAmountDividend = pageArray => {
+  let amountLineIndex = pageArray.findIndex(line =>
+    line.includes('Brutto EUR')
+  );
+  return parseGermanNum(pageArray[amountLineIndex].split(/\s+/)[2]);
+};
+
+const findSharesBuy = pageArray => {
   const sharesLineIndex =
     pageArray.findIndex(line => line.includes('ckzahl')) + 1;
   const sharesLineArray = pageArray[sharesLineIndex].split(/\s+/);
   return parseGermanNum(sharesLineArray[1]);
 };
 
-const findFee = pageArray => {
-  // For foreign buy the fees are located somewhere else
+const findSharesDividend = pageArray => {
+  const sharesLineIndex = pageArray.findIndex(line =>
+    line.includes('Menge/Währung:')
+  );
+  if (sharesLineIndex >= 0) {
+    return parseGermanNum(pageArray[sharesLineIndex].split(/\s+/)[1]);
+  }
+};
 
-  const feeIndex = pageArray.indexOf('Summe Kosten und Geb');
-  // if the transaction is in EUR
-  if (feeIndex > 0) {
-    return parseGermanNum(pageArray[feeIndex + 2].split(/\s+/)[1]);
-  }
-  // if the transaction is in another currency
-  else {
-    const sharesLineIndex = pageArray.findIndex(line =>
-      line.includes('Gesamtkosten und -gebühren')
+const findFee = (pageArray, legacyDocument = false) => {
+  // For foreign buy the fees are located somewhere else
+  let completeFee = Big(0);
+  // Legacy Documents list the fees differently and list the initial charge
+  if (legacyDocument) {
+    // Add the Ausgabeaufschlag
+    const initialChargeLine = pageArray.findIndex(line =>
+      line.includes('In der Abrechnung ist in Summe ein Ausgabeaufschlag')
     );
-    const feeLineArray = pageArray[sharesLineIndex].split(/\s+/);
-    return parseGermanNum(feeLineArray[3]);
+    if (initialChargeLine >= 0) {
+      completeFee = completeFee.plus(
+        parseGermanNum(pageArray[initialChargeLine].split(/\s+/)[10])
+      );
+    }
+    // Add the bonification
+    const bonificationLine = pageArray.findIndex(line =>
+      line.includes('Bonifikation')
+    );
+    if (bonificationLine >= 0) {
+      completeFee = completeFee.plus(
+        parseGermanNum(pageArray[bonificationLine].split(/\s+/)[3])
+      );
+    }
+  } else {
+    const feeIndex = pageArray.indexOf('Summe Kosten und Geb');
+    // if the transaction is in EUR
+    if (feeIndex > 0) {
+      return parseGermanNum(pageArray[feeIndex + 2].split(/\s+/)[1]);
+    }
+    // if the transaction is in another currency
+    else {
+      const sharesLineIndex = pageArray.findIndex(line =>
+        line.includes('Gesamtkosten und -gebühren')
+      );
+      if (sharesLineIndex >= 0) {
+        completeFee = completeFee.plus(
+          parseGermanNum(pageArray[sharesLineIndex].split(/\s+/)[3])
+        );
+      }
+    }
   }
+  return completeFee;
+};
+
+const findTaxPayout = pageArray => {
+  let completeTax = Big(0);
+  const kestThreeIndex = pageArray.findIndex(line =>
+    line.includes('KESt III pro Stück')
+  );
+  if (kestThreeIndex >= 0) {
+    completeTax = completeTax.plus(
+      Big(parseGermanNum(pageArray[kestThreeIndex].split(/\s+/)[6])).abs()
+    );
+  }
+  const kestTwoIndex = pageArray.findIndex(line =>
+    line.includes('KESt II pro Stück')
+  );
+  if (kestTwoIndex >= 0) {
+    completeTax = completeTax.plus(
+      Big(parseGermanNum(pageArray[kestTwoIndex].split(/\s+/)[6])).abs()
+    );
+  }
+  return completeTax;
 };
 
 const isBuy = pageArray => {
   return pageArray.some(line => line.includes('uf Marktplatz vom'));
 };
 
-export const canParsePage = (pageArray, extension) =>
-  extension === 'pdf' &&
-  pageArray.some(line => line.includes('ATU63358299')) &&
-  isBuy(pageArray);
+const isDividend = pageArray => pageArray.includes('Ausschüttung');
+
+const isOldBuy = pageArray => {
+  return pageArray.some(line => line.includes('Kauf aus Wertpapierliste'));
+};
+
+export const canParsePage = (pageArray, extension) => {
+  try {
+      const isErsteBankFile = pageArray
+        .join('')
+        .includes('ESTERREICHISCHENSPARKASSEN');
+    return (
+      extension === 'pdf' &&
+      (isBuy(pageArray) || isOldBuy(pageArray) || isDividend(pageArray)) &&
+      isErsteBankFile
+    );
+  }
+  catch (TypeError) {
+    return false
+  }
+};
 
 export const parsePages = content => {
+  // Flatten every incomming array
   const pdfPagesConcat = [].concat.apply([], content);
   const broker = 'ersteBank';
-  let type, amount, shares, isin, company, date, price, tax, fee
+  let type, amount, shares, isin, company, date, price, tax, fee;
 
   if (isBuy(pdfPagesConcat)) {
     type = 'Buy';
-    isin = findIsin(pdfPagesConcat);
+    isin = findIsinBuy(pdfPagesConcat);
     company = findCompany(pdfPagesConcat);
-    date = findDate(pdfPagesConcat);
-    amount = findAmount(pdfPagesConcat);
-    shares = findShares(pdfPagesConcat);
+    date = findDateBuy(pdfPagesConcat);
+    amount = findAmountBuy(pdfPagesConcat);
+    shares = findSharesBuy(pdfPagesConcat);
     price = +Big(amount).div(shares);
     tax = 0;
-    fee = findFee(pdfPagesConcat);
-    }
+    fee = +findFee(pdfPagesConcat);
+  } else if (isOldBuy(pdfPagesConcat)) {
+    type = 'Buy';
+    isin = findIsinBuy(pdfPagesConcat);
+    company = findCompany(pdfPagesConcat);
+    date = findDateBuy(pdfPagesConcat);
+    fee = +findFee(pdfPagesConcat, true);
+    amount = +Big(findAmountBuy(pdfPagesConcat, true)).minus(fee);
+    shares = findSharesBuy(pdfPagesConcat);
+    price = +Big(amount).div(shares);
+    tax = 0;
+  } else if (isDividend(pdfPagesConcat)) {
+    type = 'Dividend';
+    isin = stringAtIndexOf(pdfPagesConcat, 'Ausschüttung', 0, 2);
+    company = findCompanyDividend(pdfPagesConcat);
+    date = findDateDividend(pdfPagesConcat);
+    amount = findAmountDividend(pdfPagesConcat);
+    shares = findSharesDividend(pdfPagesConcat);
+    price = +Big(amount).div(shares);
+    fee = 0;
+    tax = +findTaxPayout(pdfPagesConcat);
+  }
 
-  let activity
-  if (isForeignCurrencyTransaction(pdfPagesConcat)) {
-    const foreignInformation = findForeignCurrencyFxRate(pdfPagesConcat);
-    activity = {
-      broker: broker,
-      type: type,
-      isin: isin,
-      company: company,
-      date: date,
-      amount: amount,
-      shares: shares,
-      price: price,
-      tax: tax,
-      fee: fee,
-      fxRate: foreignInformation[0],
-      foreignCurrency: foreignInformation[1],
-    }
+  let activity;
+  const foreignInformation = findForeignCurrencyFxRate(pdfPagesConcat);
+  activity = {
+    broker: broker,
+    type: type,
+    isin: isin,
+    company: company,
+    date: date,
+    amount: amount,
+    shares: shares,
+    price: price,
+    tax: tax,
+    fee: fee,
+  };
+
+  if (foreignInformation[0] !== undefined) {
+    activity.fxRate = foreignInformation[0];
   }
-  else {
-    activity = {
-      broker: broker,
-      type: type,
-      isin: isin,
-      company: company,
-      date: date,
-      amount: amount,
-      shares: shares,
-      price: price,
-      tax: tax,
-      fee: fee,
-    }
+  if (foreignInformation[1] !== undefined) {
+    activity.foreignCurrency = foreignInformation[1];
   }
+
   const activities = [validateActivity(activity)];
 
   return {
