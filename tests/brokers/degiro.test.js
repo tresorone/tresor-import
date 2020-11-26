0f9e94d0b537df48489e711fbdcdd8e58af4a326
David Dreher <dredav@users.noreply.github.com>
2020-11-26 06:54:47 +0100

    Add test case

--- tests/brokers/degiro.test.js
+++ tests/brokers/degiro.test.js
@@ -58,6 +58,43 @@ describe('Broker: DEGIRO', () => {
         tax: 0,
       });
     });
+
+    test('Can the transactions be parsed from: buy_sell_and_call_transactions', () => {
+      const activities = degiro.parsePages(transactionLog[1]).activities;
+
+      expect(activities.length).toEqual(28);
+      expect(
+        activities.filter(activity => activity !== undefined).length
+      ).toEqual(25);
+      expect(activities[5]).toEqual({
+        broker: 'degiro',
+        type: 'Buy',
+        date: '2019-05-31',
+        isin: 'SE0011527845',
+        company: 'QLINEA',
+        shares: 100,
+        price: 6.1153,
+        amount: 611.53,
+        fee: 4.36,
+        tax: 0,
+        fxRate: 10.6185,
+        foreignCurrency: 'SEK',
+      });
+      expect(activities[9]).toEqual({
+        broker: 'degiro',
+        type: 'Buy',
+        date: '2019-05-14',
+        isin: 'US9839191015',
+        company: 'XILINX INC. - COMMON',
+        shares: 8,
+        price: 100.90625,
+        amount: 807.25,
+        fee: 0.52,
+        tax: 0,
+        fxRate: 1.1226,
+        foreignCurrency: 'USD',
+      });
+    });
   });
 
   beforeEach(() => {
