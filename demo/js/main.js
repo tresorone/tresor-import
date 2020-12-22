663bc476e918cd500048efbf32bc7d432d99772a
David Dreher <dredav@users.noreply.github.com>
2020-12-22 06:22:05 +0100

    Demo now use `parseFile` and `parseActivitiesFromPages` instead of `getActivities`

--- demo/js/main.js
+++ demo/js/main.js
@@ -2,11 +2,9 @@ import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 import parse from 'date-fns/parse';
 import { de } from 'date-fns/locale';
 // Use the webpack version to ensure, that the published version works fine and not only the src/ one.
-import getActivities, {
-  parseActivitiesFromPages,
-} from '../bundle/tresor-import';
+import { parseFile, parseActivitiesFromPages } from '../bundle/tresor-import';
 // To use the published version, uncomment the following line after running: npm run build
-// import getActivities, { parseActivitiesFromPages } from '../../dist/tresor-import';
+// import { parseFile, parseActivitiesFromPages } from '../../dist/tresor-import';
 
 new Vue({
   el: '#app',
@@ -99,12 +97,22 @@ new Vue({
       copyText.style.display = 'none';
     },
     async fileHandler() {
-      const results = await Promise.all(
-        Array.from(this.$refs.myFiles.files).map(getActivities)
-      );
-
       this.clearResults();
-      results.forEach(this.handleParserResults);
+      Array.from(this.$refs.myFiles.files).forEach(file => {
+        parseFile(file).then(parsedFile => {
+          const result = parseActivitiesFromPages(
+            parsedFile.pages,
+            parsedFile.extension
+          );
+
+          result.file = file.name;
+          result.content = parsedFile.pages;
+          result.successful =
+            result.activities !== undefined && result.status === 0;
+
+          this.handleParserResults(result);
+        });
+      });
     },
     clearResults() {
       this.errors = [];
